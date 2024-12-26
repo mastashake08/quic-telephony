39b87aca121dbf0351caede48e9758e01421df05
Jyrone Parker <jyrone.parker@gmail.com>
2024-12-26 07:40:46 -0500

    run chrome with flags on chrome

---
+++ mac-chrome-dev-chrome.sh
@@ -0,0 +1,5 @@
+/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome \ 
+  --enable-experimental-web-platform-features \
+  --ignore-certificate-errors-spki-list=ezBxTp/HQOqJQfnE61J/cp70BhS75b6pc0myDGSwL24= \
+  --origin-to-force-quic-on=localhost:4433 \
+ http://localhost:3000/ \ --user-data-dir=/tmp
