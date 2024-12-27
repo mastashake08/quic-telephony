854706b8cbb24c82810fabdc86f63fb27bd8ec8d
Jyrone Parker <jyrone.parker@atlasied.com>
2024-12-27 16:19:47 -0500

    add workspace

--- mac-chrome-dev-chrome.sh
+++ mac-chrome-dev-chrome.sh
@@ -2,5 +2,4 @@
   --enable-experimental-web-platform-features \
   --ignore-certificate-errors-spki-list=ezBxTp/HQOqJQfnE61J/cp70BhS75b6pc0myDGSwL24= \
   --origin-to-force-quic-on=192.168.68.132:4433 \
- http://localhost:3000/ \ --user-data-dir=/tmp \
- --kiosk
+ http://localhost:3000/ 
